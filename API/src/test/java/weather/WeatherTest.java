808b15f84511fafb5792ac83208981e7379b4cc8
Dmitry Tyryshkin <taskme@yandex-team.ru>
2018-02-01 19:52:51 +0300

    Rename class

--- API/src/test/java/weather/WeatherTest.java
+++ API/src/test/java/weather/WeatherTest.java
@@ -34,7 +34,7 @@ public class WeatherTest {
     public void checkCity() {
         when()
                 .request(GET, "http://api.openweathermap.org/data/2.5/weather?id={ID}&appid=c5ce4d8d7647d5a3a8046e6a33605f90", idCity) //PROD
-                //.request(GET, "http://samples.openweathermap.org/data/2.5/weather?id={cityID}&appid=b6907d289e10d714a6e88b30761fae22", number ) //TEST ONLY
+                //.request(GET, "http://samples.openweathermap.org/data/2.5/weather?id={cityID}&appid=b6907d289e10d714a6e88b30761fae22", number ) //TEST ONLY.
                 .then()
                 .statusCode(200)
                 .body("name", is(expCity), "sys.country", is(expCountry));
